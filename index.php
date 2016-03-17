7f2797461ff4f22927b74b382f5949de7e35e1b9
okorani <okorani@okorani>
2016-03-17 10:00:55 +0200

    modifications

--- index.php
+++ index.php
@@ -20,56 +20,67 @@ if (!isset($_SESSION["lang"]) || ($_SESSION["lang"] == "ar")) {
     </head>
     <body>
         <div class="container">
-
-            <nav class="navbar navbar-inverse navbar-static-top">
-                <div class="container-fluid">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#"><?php echo LANG_index_navbarBrand ?></a>
-                    </div>
-
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-                        <ul class="nav navbar-nav">
-                            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                            <li><a href="#"><?php echo LANG_index_share ?></a></li>
-                        </ul>
-
-                        <ul class="nav navbar-nav navbar-right">
-                            <li data-toggle="modal" data-target="#language"><a href="#">Language</a></li>
-                        </ul>
-                    </div><!-- /.navbar-collapse -->
-                </div><!-- /.container-fluid -->
-            </nav>
-
+            
             <div class="panel panel-default" style="text-align: center;">
+                
+                <nav class="navbar navbar-inverse navbar-static-top">
+                    <div class="container-fluid">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="#"><?php echo LANG_index_navbarBrand ?></a>
+                        </div>
+
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                            <ul class="nav navbar-nav">
+                                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                                <li><a href="#"><?php echo LANG_index_share ?></a></li>
+                            </ul>
+
+                            <ul class="nav navbar-nav navbar-right">
+                                <li data-toggle="modal" data-target="#language"><a href="#">Language</a></li>
+                            </ul>
+                        </div><!-- /.navbar-collapse -->
+                    </div><!-- /.container-fluid -->
+                </nav>
+
+
                 <div class="panel-body">
-                    <h1><?php echo LANG_index_SiteTitle ?></h1>
+                    <img src="public/img/logo.png" width="128p">
+                    <h3><?php echo LANG_index_SiteTitle ?></h3>
                     <div class="pager"></div>
                     <p><?php echo LANG_index_mainMessage ?></p><br><br>
-                    <a class="btn btn-info btn-lg"><?php echo LANG_index_JoinUs ?></a>
+                    
                 </div>
+                
+                
                 <div class="row" style="padding: 25px;">
                     <div class="col-sm-6">
-                        <div class="panel panel-default">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <?php echo LANG_index_PathFinderLink ?>
+                            </div>
                             <div class="panel-body">
                                 <p><?php echo LANG_index_PathFinderMessage; ?></p>
-                                <a class="btn btn-success btn-lg btn-block" href="pathfinder.php"><?php echo LANG_index_PathFinderLink ?></a>
+                                <!-- <a class="btn btn-default btn-lg btn-block" href="pathfinder.php"><?php echo LANG_index_PathFinderLink ?></a> -->
                             </div>
                         </div>
                     </div>
                     <div class="col-sm-6">
-                        <div class="panel panel-default">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <?php echo LANG_index_SearchTrafficLink ?>
+                            </div>
                             <div class="panel-body">
                                 <p><?php echo LANG_index_SearchTrafficMessage; ?></p>
-                                <a class="btn btn-success btn-lg btn-block" href="searchtraffic.php"><?php echo LANG_index_SearchTrafficLink ?></a>
+                                <!-- <a class="btn btn-success btn-lg btn-block" href="searchtraffic.php"><?php echo LANG_index_SearchTrafficLink ?></a> -->
                             </div>
                         </div>
                     </div>
@@ -82,7 +93,7 @@ if (!isset($_SESSION["lang"]) || ($_SESSION["lang"] == "ar")) {
 
         <!-- include Models -->
         <?php require_once 'public/views/models.html'; ?>
-        
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="public/js/bootstrap.min.js"></script>
     </body>
